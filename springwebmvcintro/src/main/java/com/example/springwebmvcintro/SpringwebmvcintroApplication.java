25a36c0cab676f2f6ce32bc151441855a93d736f
JoowonSuh <sjw2023@naver.com>
2022-09-22 18:27:36 +0900

    first commit

---
+++ springwebmvcintro/src/main/java/com/example/springwebmvcintro/SpringwebmvcintroApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebmvcintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebmvcintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebmvcintroApplication.class, args);
+	}
+
+}
---
+++ springwebmvcintro/src/test/java/com/example/springwebmvcintro/SpringwebmvcintroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebmvcintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebmvcintroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

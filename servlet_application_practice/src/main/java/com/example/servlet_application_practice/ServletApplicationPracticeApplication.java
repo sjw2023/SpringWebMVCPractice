94f31ed1997045be69c2393c18550824b7d748fb
JoowonSuh <sjw2023@naver.com>
2022-09-24 18:43:12 +0900

    서블릿 어플리케이션 예재

---
+++ servlet_application_practice/src/main/java/com/example/servlet_application_practice/ServletApplicationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.servlet_application_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplicationPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplicationPracticeApplication.class, args);
+	}
+
+}
---
+++ servlet_practice/src/main/java/com/example/servlet_practice/HelloServlet.java
@@ -0,0 +1,5 @@
+package com.example.servlet_practice;
+
+public class HelloServlet extends HttpSer{
+    
+}

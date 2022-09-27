5af5e27b45e71111f041ebbf3da8c0df326a4f60
JoowonSuh <sjw2023@naver.com>
2022-09-28 00:00:33 +0900

    Spring-boot with JSP

---
+++ spring_with_jsp/src/main/java/com/example/spring_with_jsp/SpringWithJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_with_jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithJspApplication.class, args);
+	}
+
+}

fa8f19dcaba2330e60e28ada7f27c7b3b42551e3
JoowonSuh <sjw2023@naver.com>
2022-09-27 16:27:52 +0900

    필터 매핑 이후 동작 확인

--- servlet_application_maven_archetype_webapp_practice/src/main/java/com/example/MyFilter.java
+++ servlet_application_maven_archetype_webapp_practice/src/main/java/com/example/MyFilter.java
@@ -18,7 +18,7 @@ public class MyFilter implements Filter{
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws UnsupportedOperationException, IOException, ServletException{
         System.out.println("Filter");//그냥 이것만 두면 출력안됨 FilterChain 파라미터를 이용해 다음 필터로 넘겨줘야함
-        chain.doFilter(request, response);// 다음 필터로 연결해주는 부분
+        chain.doFilter(request, response);// 다음 필터로 연결해주는 부분, 맨 마지막 필터는 알아서 서블릿으로 연결해줌
     }
     @Override
     public void destroy(){

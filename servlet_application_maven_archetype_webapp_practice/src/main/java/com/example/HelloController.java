bc2eb35112d25b6d6dd785a6be1b609dfb631275
JoowonSuh <sjw2023@naver.com>
2022-09-27 19:49:34 +0900

    디스패쳐 서블릿 동작원리 2

--- servlet_application_maven_archetype_webapp_practice/src/main/java/com/example/HelloController.java
+++ servlet_application_maven_archetype_webapp_practice/src/main/java/com/example/HelloController.java
@@ -1,10 +1,14 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//디스패쳐 서블릿 동작원리 1
+// @RestController
+@Controller
 public class HelloController {
     //빈 주입
     @Autowired
@@ -13,8 +17,14 @@ public class HelloController {
     //HelloServlet에 매핑되어있던 요청 매핑해줄 핸들러 작성
     //스프링 MVC가 이 핸들러 쪽으로 요청을 디스패치 해줄, 스프링 애노테이션을 이해하는, 리턴 스트링을 응답으로 만들어줄, 디스패치 서블릿이 필요
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(){
         return "Hello, " + helloService.getName() + "with DispatcherServlet";
+    }
+
+    @GetMapping("/sample")
+    public String sample(){
+        return "WEB-INF/samlple.jsp";S
     }
 
     

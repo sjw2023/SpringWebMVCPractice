0acbc05a27c1f016ebc8a46549a85637821979e2
JoowonSuh <sjw2023@naver.com>
2022-09-27 21:46:14 +0900

    스프링 웹 MVC 디펜던시 추가

--- spring_mvc_config/src/main/java/com/example/HelloController.java
+++ spring_mvc_config/src/main/java/com/example/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RestController;
 
 //디스패쳐 서블릿 동작원리 1
 // @RestController

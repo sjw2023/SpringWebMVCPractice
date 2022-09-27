6b3071b2e077e4210c9c1c8509246d6ef3de00b3
JoowonSuh <sjw2023@naver.com>
2022-09-27 16:17:55 +0900

    필터 클래스 추가

---
+++ servlet_application_maven_archetype_webapp_practice/src/main/java/com/example/MyFilter.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.io.IOException;
+import java.rmi.ServerException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyFilter implements Filter{
+    @Override
+    public void init(FilterConfig filterConfig ) throws ServletException{
+        System.out.println("Filter Init");
+    }
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws UnsupportedOperationException, IOException, ServletException{
+        System.out.println("Filter");//그냥 이것만 두면 출력안됨 FilterChain 파라미터를 이용해 다음 필터로 넘겨줘야함
+        chain.doFilter(request, response);// 다음 필터로 연결해주는 부분
+    }
+    @Override
+    public void destroy(){
+        System.out.println("Filter Destroy");
+
+    }
+    
+}

e157e472a941688833ee8405cb75f8cebf89b1db
JoowonSuh <sjw2023@naver.com>
2022-09-28 01:31:52 +0900

    포매터 설정 연습 예제

---
+++ fomatter_config/src/main/java/com/example/fomatter_config/FomatterConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.fomatter_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FomatterConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FomatterConfigApplication.class, args);
+	}
+
+}
---
+++ fomatter_config/src/test/java/com/example/fomatter_config/FomatterConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fomatter_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FomatterConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

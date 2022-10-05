02e1550db72657579b14d78a2b61b35a90feffc3
JoowonSuh <sjw2023@naver.com>
2022-10-05 23:18:46 +0900

    핸들러를 GET, PUT에 매핑

--- http_request_mapping/src/main/java/com/example/http_request_mapping/SampleController.java
+++ http_request_mapping/src/main/java/com/example/http_request_mapping/SampleController.java
@@ -12,7 +12,9 @@ public class SampleController {
     //핸들러를 hello라는 요청에 매핑 하고 싶을때
     // @RequestMapping("/hello")
     //GET Http Method만 허용 하고 싶으면
-    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    // @RequestMapping(value="/hello", method = RequestMethod.GET)
+    //GET과 PUT모두 허용 하고싶을때 배열을 사용
+    @RequestMapping(value="/hello", method = {RequestMethod.GET, RequestMethod.PUT})
     //리턴 문자열을 응담으로 보내고 싶을때 붙이는 어노테이션
     @ResponseBody
     public String hello(){

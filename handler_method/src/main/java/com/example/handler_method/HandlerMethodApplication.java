32474c6bab891e2deeae6d0c36853d555cd53ff0
JoowonSuh <sjw2023@naver.com>
2022-10-15 12:31:30 +0900

    핸들러 메소드 시작

---
+++ handler_method/src/main/java/com/example/handler_method/HandlerMethodApplication.java
@@ -0,0 +1,13 @@
+package com.example.handler_method;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlerMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlerMethodApplication.class, args);
+	}
+
+}
---
+++ handler_method/src/test/java/com/example/handler_method/HandlerMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handler_method;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlerMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
